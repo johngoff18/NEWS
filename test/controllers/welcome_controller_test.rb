086c613e7e6adc79f6daa52e07eb66d9e0324de8
Lyle Goff <johngoff18@gmail.com>
2019-05-16 07:52:44 -0400

    initial commit

---
+++ test/controllers/welcome_controller_test.rb
@@ -0,0 +1,14 @@
+require 'test_helper'
+
+class WelcomeControllerTest < ActionDispatch::IntegrationTest
+  test "should get index" do
+    get welcome_index_url
+    assert_response :success
+  end
+
+  test "should get search" do
+    get welcome_search_url
+    assert_response :success
+  end
+
+end
