0b53cd67063a0487dce890e8c503d60bc97dcc4e
Lyle Goff <johngoff18@gmail.com>
2019-05-16 23:07:56 -0400

    updated css

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,28 @@
 //= require activestorage
 //= require turbolinks
 //= require_tree .
+	$(document).on("turbolinks:click", function(){
+	  $(".loading").show();
+	});
+
+	$(document).on("turbolinks:load", function(){
+		$(".loading").hide();
+  	var words = [
+  	  {text: "Lorem", weight: 13},
+  	  {text: "Ipsum", weight: 10.5},
+  	  {text: "Dolor", weight: 9.4},
+  	  {text: "Sit", weight: 8},
+  	  {text: "Amet", weight: 6.2},
+  	  {text: "Consectetur", weight: 5},
+  		{text: "Adipiscing", weight: 5},
+  	  {text: "Lorem", weight: 13},
+  	  {text: "Ipsum", weight: 10.5},
+  	  {text: "Dolor", weight: 9.4},
+  	  {text: "Sit", weight: 8},
+  	  {text: "Amet", weight: 6.2},
+  	  {text: "Consectetur", weight: 5},
+  		{text: "Adipiscing", weight: 5}
+  	];
+  	$('#keywords').jQCloud(words);	  
+	});
+	
