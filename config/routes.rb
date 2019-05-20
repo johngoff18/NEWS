967b3c3015f21828ef1fa38328c7c35a3ea75df1
Lyle Goff <johngoff18@gmail.com>
2019-05-19 22:46:51 -0400

    added weather and map api

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :authors
   resources :articles
   resources :categories
   get 'welcome/index'
   get 'welcome/search'
+  get 'welcome/weather'
+  get 'welcome/map'
   
   root 'welcome#index'
 end
